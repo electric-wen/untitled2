2f167f158a51857fc3844b06e81e22ed742d0efd
electric-wen <649356875@qq.com>
2019-08-16 23:28:18 +0800

    test github

---
+++ src/com/pingan/IdeaTest.java
@@ -0,0 +1,11 @@
+package com.pingan;
+
+public class IdeaTest {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println(i);
+        }
+    }
+}
