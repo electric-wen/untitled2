d0400248e9eb8798d9d36c7bd60707b42c6c5392
electric-wen <649356875@qq.com>
2019-08-17 12:12:10 +0800

    test

---
+++ mavendemo/src/main/java/com/pingan/mavendemo/MavendemoApplication.java
@@ -0,0 +1,17 @@
+package com.pingan.mavendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 1、maven配置
+ *
+ */
+@SpringBootApplication
+public class MavendemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavendemoApplication.class, args);
+    }
+
+}
